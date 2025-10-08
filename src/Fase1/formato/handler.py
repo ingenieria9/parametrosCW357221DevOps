5fd20fbbc298201c6157a4b9c6470f7380072509
ingenieria9 <ingenieria9@telemetrik.com.co>
2025-10-08 09:13:30 -0500

    edit formato fase 1 lambda to test db connection

--- src/Fase1/formato/handler.py
+++ src/Fase1/formato/handler.py
@@ -1,16 +1,25 @@
 import boto3
 import json
+import os
+
+client = boto3.client("lambda", region_name="us-east-1") 
 
 def lambda_handler(event, context):
     
-    client = boto3.client("lambda", region_name="us-east-1")  # 👈 región donde vive la DB Access
     db_access_arn = os.environ["DB_ACCESS_LAMBDA_ARN"]
 
     response = client.invoke(
-        FunctionName=db_access_arn,
-        InvocationType="RequestResponse",
-        Payload=json.dumps({"action": "query", "data": "..."})
+        FunctionName=db_access_arn,  # ARN completo
+        InvocationType="RequestResponse",  # o 'Event' si no quieres esperar respuesta
+        Payload=json.dumps({
+            {
+            "queryStringParameters": {
+                "query": "SELECT * FROM puntos_capa_principal",
+                "time_column": "timestamp",
+                "db_name": "parametros"
+            }
+            }
+        })
     )
-
-    result = json.load(response["Payload"])
+    result = json.load(response)
     return result
